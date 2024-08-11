e73d33c77a4e6f345833e8ac93eb2f49120b0709
Tyler Downing <tyler.ar.downing@hotmail.com>
2024-08-11 18:23:41 -0300

    Finshed the basic API

---
+++ ToDo_App/ToDo_List_api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Todo
+
+class ToDoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'detail', 'created_date', 'due_date', 'completed')
+
+
+        
+    
+    
+
--- ToDo_App/ToDo_List_api/views.py
+++ ToDo_App/ToDo_List_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Todo
+from .serializers import ToDoSerializer
+#from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+# Didn't add, but could just add LoginRequiredMixin, this would only show page if you are logedIn. Also, should filter only specific users Todo's 
+class ToDoView(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = ToDoSerializer
+
