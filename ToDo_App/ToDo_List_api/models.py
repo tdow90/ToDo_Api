a7baee806d8fff733786f6eec57f30af2e55a325
Tyler Downing <tyler.ar.downing@hotmail.com>
2024-08-10 22:15:05 -0300

    First Commit for the todo app

---
+++ ToDo_App/ToDo_List_api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    detail = models.TextField(blank='True')
+    created_date = models.DateTimeField(auto_now_add="True")
+    due_date = models.DateField()
+    completed = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+    
